1a11ccab9f58cb494e0287adf04f31399a7bf25c
Maciej Rozewicz <rozewicz.maciej@gmail.com>
2023-06-13 23:51:57 +0200

    Added conversion from radar scan to cartesian points

--- core/include/barrier_tracker.hpp
+++ core/include/barrier_tracker.hpp
@@ -1,6 +1,10 @@
 #ifndef CORE_INCLUDE_BARRIER_TRACKER_HPP_
 #define CORE_INCLUDE_BARRIER_TRACKER_HPP_
 
+#include <vector>
+
+#include <eigen3/Eigen/Dense>
+
 #include "barriers.hpp"
 #include "calibrations.hpp"
 #include "radar_scan.hpp"
@@ -15,8 +19,16 @@ namespace se {
       const Barriers & GetBarriers(void) const;
 
     private:
+      struct CartesianPoint {
+        Eigen::Vector2f point;
+        Eigen::Matrix2f covariance;
+      };
+
+      void ConvertDetectionsToCartesianPoints(const RadarScan & radar_scan);      
+
       Barriers output_barriers_;
       Calibrations calibrations_;
+      std::vector<CartesianPoint> points_cartesian_;
   };
 } //  namespace se
 
--- core/src/barrier_tracker.cpp
+++ core/src/barrier_tracker.cpp
@@ -1,14 +1,43 @@
 #include "barrier_tracker.hpp"
 
+#include <algorithm>
+#include <cmath>
+
 namespace se {
   BarrierTracker::BarrierTracker(const Calibrations & calibrations)
     : calibrations_{calibrations} {}
 
   void BarrierTracker::Run(const RadarScan & radar_scan) {
-    //
+    // Convert detections
+    ConvertDetectionsToCartesianPoints(radar_scan);
   }
 
   const Barriers & BarrierTracker::GetBarriers(void) const {
     return output_barriers_;
+  }
+
+  void BarrierTracker::ConvertDetectionsToCartesianPoints(const RadarScan & radar_scan) {
+    // Clear old points
+    points_cartesian_.clear();
+
+    // Convert detections
+    std::transform(radar_scan.detections.begin(), radar_scan.detections.end(),
+      std::back_inserter(points_cartesian_),
+      [](const RadarDetection & radar_detection) -> CartesianPoint {
+        CartesianPoint cartesian_point;
+
+        const auto c = std::cos(radar_detection.azimuth);
+        const auto s = std::sin(radar_detection.azimuth);
+
+        // Pose
+        cartesian_point.point(0u) = c * radar_detection.range;
+        cartesian_point.point(1u) = s * radar_detection.range;
+
+        // Covariance
+        cartesian_point.covariance(0u, 0u) = std::pow(c * radar_detection.range_std, 2) + std::pow(s * radar_detection.range * radar_detection.azimuth_std, 2);
+        cartesian_point.covariance(0u, 1u) = cartesian_point.covariance(1u, 0u) = c * s * (std::pow(radar_detection.range_std, 2) + std::pow(radar_detection.range * radar_detection.azimuth_std, 2));
+        cartesian_point.covariance(1u, 1u) = std::pow(s * radar_detection.range_std, 2) + std::pow(c * radar_detection.range * radar_detection.azimuth_std, 2);
+      }
+    );
   }
 } //  namespace se
